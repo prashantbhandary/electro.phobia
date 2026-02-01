cffa28e0846202fc93a90758cc59c0cdf1641dee
prashantbhandary <santhprashant@gmail.com>
2026-02-01 23:02:17 +0545

    fix shop

--- app/shop/[id]/layout.tsx
+++ app/shop/[id]/layout.tsx
@@ -1,8 +1,9 @@
 import { Metadata } from 'next'
 import { generateProductMetadata } from '@/lib/metadata'
 
-export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
-  return await generateProductMetadata(params.id)
+export async function generateMetadata({ params }: { params: Promise<{ id: string }> }): Promise<Metadata> {
+  const { id } = await params
+  return await generateProductMetadata(id)
 }
 
 export default function ProductLayout({ children }: { children: React.ReactNode }) {
